ae41708091b605043eb8854d8b24c32e5ff37fa9
AMOREM <amorem@amorem.com>
2019-08-16 09:01:12 +0200

    Using Gradients

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,8 +1,21 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import '../models/app_state.dart';
+
+final gradientBackground = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment.bottomLeft,
+    end: Alignment.topRight,
+    stops: [0.1, 0.3, 0.5, 0.7, 0.9],
+    colors: [
+      Colors.deepOrange[300],
+      Colors.deepOrange[400],
+      Colors.deepOrange[500],
+      Colors.deepOrange[600],
+      Colors.deepOrange[700]
+    ],
+  ),
+);
 
 class ProductsPage extends StatefulWidget {
   static const routeName = "/products";
@@ -25,25 +38,45 @@ class _ProductsPageState extends State<ProductsPage> {
   final _appBar = PreferredSize(
     preferredSize: Size.fromHeight(60),
     child: StoreConnector<AppState, AppState>(
-      converter: (store) => store.state,
-      builder: (context, state) {
-        return AppBar(
-          centerTitle: true,
-          title: SizedBox(child: state.user !=null ? Text(state.user.username) : Text(""),),
-          leading: Icon(Icons.store),
-          actions: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(right: 12.0),
-              child: state.user != null ? IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {},) : Text(''),
-            )
-          ],
-        );
-      }
-    ),
-    );
+        converter: (store) => store.state,
+        builder: (context, state) {
+          return AppBar(
+            centerTitle: true,
+            title: SizedBox(
+              child: state.user != null ? Text(state.user.username) : Text(""),
+            ),
+            leading: Icon(Icons.store),
+            actions: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 12.0),
+                child: state.user != null
+                    ? IconButton(
+                        icon: Icon(Icons.exit_to_app),
+                        onPressed: () {},
+                      )
+                    : Text(''),
+              )
+            ],
+          );
+        }),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: _appBar, body: Container(child: Text("Product Pages"),),)
+    return Scaffold(
+      appBar: _appBar,
+      body: Container(
+        decoration: gradientBackground,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text("Product Pages"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
